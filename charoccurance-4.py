b44eeb2efe5dd8d70118fe91a1bd0ae1280e6e89
sabaritamil812001 <49053332+sabaritamil812001@users.noreply.github.com>
2021-09-02 21:46:30 +0530

    Add files via upload

---
+++ charoccurance-4.py
@@ -0,0 +1,12 @@
+def check(word):
+    new=word
+    for i in range(len(word)):
+        count=word.count(word[i])
+        if count>=2:
+            occur=word.find(word[i])
+            new=word[:occur+1]+str(count)+word[occur+count:]
+    print(new,end=" ")
+
+text=list(map(str,input().split()))
+for i in range(len(text)):
+    check(text[i])
